ecba540a8c9f1d08659ad8f2363d3414aa281122
nkizito92 <nkizito57@gmail.com>
2020-02-07 10:30:36 -0600

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,20 @@
 class Song < ActiveRecord::Base
-  # add associations here
+  belongs_to :artist
+  belongs_to :genre
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def genre_id(id)
+    self.genre = Genre.find_or_create_by(id: id)
+  end
+
+  def genre_id
+    self.genre ? self.genre.id : nil
+  end
+
+  def artist_name
+    self.artist ? self.artist.name : nil
+  end 
 end
